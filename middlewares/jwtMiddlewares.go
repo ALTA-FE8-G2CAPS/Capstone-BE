0e83d1722f061d8f23f456ab1c596cfa4958a256
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-09-28 20:42:29 +0700

    Add JWT

--- middlewares/jwtMiddlewares.go
+++ middlewares/jwtMiddlewares.go
@@ -1,1 +1,55 @@
 package middlewares
+
+import (
+	"capstone-project/config"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	return middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: "HS256",
+		SigningKey:    []byte(config.SECRET_JWT),
+	})
+}
+
+func CreateToken(userId int, userRole string, userName string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userId
+	claims["user_role"] = userRole
+	claims["user_name"] = userName
+
+	claims["exp"] = time.Now().Add(time.Hour * 12).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(config.SECRET_JWT))
+}
+
+func ExtractToken(c echo.Context) int {
+	user := c.Get("user").(*jwt.Token)
+	if user.Valid {
+		claims := user.Claims.(jwt.MapClaims)
+		userId := claims["userId"].(float64)
+		return int(userId)
+	}
+	return -1
+}
+
+func ExtractClaims(tokenStr string) (jwt.MapClaims, bool) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, nil
+		}
+		return []byte(config.SECRET_JWT), nil
+	})
+	if err != nil {
+		return nil, false
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, true
+	}
+	return nil, false
+}
