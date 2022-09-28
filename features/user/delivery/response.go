adb5fd88d52efb7e78060974df590fe51a3455ba
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-09-28 23:35:53 +0700

    Add user-handler-get

--- features/user/delivery/response.go
+++ features/user/delivery/response.go
@@ -8,7 +8,6 @@ type UserResponse struct {
 	ID           uint   `json:"id" form:"id"`
 	Nama_User    string `json:"nama_user" form:"nama_user"`
 	Email        string `json:"email" form:"email"`
-	Password     string `json:"password" form:"password"`
 	Role         string `json:"role" form:"role"`
 	Address_user string `json:"address_user" form:"address_user"`
 	Foto_user    string `json:"foto_user" form:"foto_user"`
@@ -20,7 +19,6 @@ func FromCore(data user.UserCore) UserResponse {
 		ID:           data.ID,
 		Nama_User:    data.Nama_User,
 		Email:        data.Email,
-		Password:     data.Password,
 		Role:         data.Role,
 		Address_user: data.Address_user,
 		Foto_user:    data.Foto_user,
