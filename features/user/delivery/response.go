1d6f59881dc7a5190f0f4a3c8817641e33d9087f
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-09-28 19:41:09 +0700

    Add user-delivery-response

---
+++ features/user/delivery/response.go
@@ -0,0 +1,37 @@
+package delivery
+
+import (
+	"capstone-project/features/user"
+)
+
+type UserResponse struct {
+	ID           uint   `json:"id" form:"id"`
+	Nama_User    string `json:"nama_user" form:"nama_user"`
+	Email        string `json:"email" form:"email"`
+	Password     string `json:"password" form:"password"`
+	Role         string `json:"role" form:"role"`
+	Address_user string `json:"address_user" form:"address_user"`
+	Foto_user    string `json:"foto_user" form:"foto_user"`
+	User_owner   string `json:"user_owner" form:"user_owner"`
+}
+
+func FromCore(data user.UserCore) UserResponse {
+	return UserResponse{
+		ID:           data.ID,
+		Nama_User:    data.Nama_User,
+		Email:        data.Email,
+		Password:     data.Password,
+		Role:         data.Role,
+		Address_user: data.Address_user,
+		Foto_user:    data.Foto_user,
+		User_owner:   data.User_owner,
+	}
+}
+
+func FromCoreList(data []user.UserCore) []UserResponse {
+	var list []UserResponse
+	for _, v := range data {
+		list = append(list, FromCore(v))
+	}
+	return list
+}
