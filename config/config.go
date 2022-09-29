c6d31c32bb05ae694edc5f509167841097c74fa2
raorafarhan <raorafarhan@gmail.com>
2022-09-29 10:48:24 +0700

    add-config

--- config/config.go
+++ config/config.go
@@ -1,2 +1,66 @@
 package config
 
+import (
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	// "github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	DB_DRIVER   string
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_HOST     string
+	DB_PORT     int
+	DB_NAME     string
+	SERVER_PORT int
+}
+
+var lock = &sync.Mutex{}
+var appConfig *AppConfig
+
+func GetConfig() *AppConfig {
+	lock.Lock()
+	defer lock.Unlock()
+
+	if appConfig == nil {
+		appConfig = initConfig()
+
+	}
+
+	return appConfig
+}
+
+func initConfig() *AppConfig {
+	var defConfig AppConfig
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	defConfig.DB_USERNAME = os.Getenv("DB_USERNAME")
+	defConfig.DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	defConfig.DB_HOST = os.Getenv("DB_HOST")
+	defConfig.DB_NAME = os.Getenv("DB_NAME")
+
+	dbPortConv, errDBPort := strconv.Atoi(os.Getenv("DB_PORT"))
+
+	if errDBPort != nil {
+		log.Fatal("error parse DB PORT")
+		return nil
+	}
+
+	defConfig.DB_PORT = dbPortConv
+
+	serverPortConv, errServerPort := strconv.Atoi(os.Getenv("SERVER_PORT"))
+
+	if errServerPort != nil {
+		log.Fatal("error parse SERVER PORT")
+		return nil
+	}
+
+	defConfig.SERVER_PORT = serverPortConv
+
+	return &defConfig
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 )
 
 require (
