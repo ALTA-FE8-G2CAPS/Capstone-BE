c3b4f4a17fb3740b89286a8938b50f93809420b8
raorafarhan <raorafarhan@gmail.com>
2022-09-30 22:55:59 +0700

    venue-delivery-handler-create

--- features/venue/delivery/request.go
+++ features/venue/delivery/request.go
@@ -5,6 +5,7 @@ import "capstone-project/features/venue"
 type VenueRequest struct {
 	Name_venue        string  `json:"name_venue" form:"name_venue"`
 	Address_venue     string  `json:"address_venue" form:"address_venue"`
+	UserID            uint    `json:"user_id" form:"user_id"`
 	Latitude          float64 `json:"latitude" form:"latitude"`
 	Longitude         float64 `json:"longitude" form:"longitude"`
 	Description_venue string  `json:"description_venue" form:"description_venue"`
@@ -26,6 +27,7 @@ func ToCore(data VenueRequest) venue.VenueCore {
 	return venue.VenueCore{
 		Name_Venue:        data.Name_venue,
 		Address_Venue:     data.Address_venue,
+		UserID:            data.UserID,
 		Latitude:          data.Latitude,
 		Longitude:         data.Longitude,
 		Description_venue: data.Description_venue,
