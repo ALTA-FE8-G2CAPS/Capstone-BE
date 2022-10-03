c9e05917912fe14386f2951c45b1b03dfb608c82
AdityoGunawan <muhammadadityogunawan@gmail.com>
2022-10-03 17:56:00 +0700

    Add field-usecase-getbyid

--- features/field/entity.go
+++ features/field/entity.go
@@ -9,7 +9,7 @@ type FieldCore struct {
 
 type UsecaseInterface interface {
 	GetAllField(venue_id int) (data []FieldCore, er error)
-	// GetFieldById(id int) (data FieldCore, err error)
+	GetFieldById(id int) (data FieldCore, err error)
 	PostData(data FieldCore) (row int, err error)
 	// PutData(data FieldCore) (row int, err error)
 	// DeleteField(id int) (row int, err error)
@@ -17,7 +17,7 @@ type UsecaseInterface interface {
 
 type DataInterface interface {
 	SelectAllField() (data []FieldCore, err error)
-	// SelectFieldById(id int) (data FieldCore, err error)
+	SelectFieldById(id int) (data FieldCore, err error)
 	InserData(data FieldCore) (row int, err error)
 	// UpdateField(data FieldCore) (row int, err error)
 	// DeleteField(id int) (row int, err error)
