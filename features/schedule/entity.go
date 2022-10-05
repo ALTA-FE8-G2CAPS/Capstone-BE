81b17e31259c2b2c88368259b60449f85edb14a6
raorafarhan <raorafarhan@gmail.com>
2022-10-05 14:46:52 +0700

    schedule-usecase

--- features/schedule/entity.go
+++ features/schedule/entity.go
@@ -5,21 +5,21 @@ type ScheduleCore struct {
 	FieldID     uint
 	Category    string
 	Day         string
-	Start_hours uint
-	End_hours   uint
+	Start_hours string
+	End_hours   string
 }
 
 type UsecaseInterface interface {
-	GetAllField(field_id int) (data []ScheduleCore, err error)
-	GetFieldById(id int) (data ScheduleCore, err error)
+	GetAllSchedule(field_id int) (data []ScheduleCore, err error)
+	GetScheduleById(id int) (data ScheduleCore, err error)
 	PostData(data ScheduleCore) (row int, err error)
 	// PutData(data FieldCore, user_id int) (row int, err error)
 	// DeleteField(user_id, field_id int) (row int, err error)
 }
 
 type DataInterface interface {
-	SelectAllField(field_id int) (data []ScheduleCore, err error)
-	SelectFieldById(id int) (data ScheduleCore, err error)
+	SelectAllSchedule(field_id int) (data []ScheduleCore, err error)
+	SelectScheduleById(id int) (data ScheduleCore, err error)
 	InsertData(data ScheduleCore) (row int, err error)
 	// UpdateField(data FieldCore, user_id int) (row int, err error)
 	// DeleteField(user_id, field_id int) (row int, err error)
